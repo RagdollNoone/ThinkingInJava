7882a7f49b376fdf7e5daef6934906a42945adf0
RagdollNoone <RagdollNoone@163.com>
2018-05-29 17:26:38 +0800

    add thirparty code

--- Chapter22/TextArea.java
+++ Chapter22/TextArea.java
@@ -1,4 +1,43 @@
 package Chapter22;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import thirdparty.net.mindview.util.*;
 
-public class TextArea {
+public class TextArea extends JFrame{
+    private JButton b = new JButton("Add Data");
+    private JButton c = new JButton("Clear Data");
+
+    private JTextArea t = new JTextArea(20, 40);
+    private HashMap<String, String> m = new HashMap<String, String>();
+
+    public TextArea(){
+        m.putAll(Countries.capitals());
+        b.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                for (Map.Entry me : m.entrySet())
+                    t.append(me.getKey() + ": " + me.getValue() + "\n");
+            }
+        });
+
+        c.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                t.setText("");
+            }
+        });
+
+        setLayout(new FlowLayout());
+
+        add(new JScrollPane(t));
+        add(b);
+        add(c);
+    }
+
+    public static void main(String[] args){
+        SwingConsole.run(new TextArea(), 475, 425);
+    }
+
 }
