77da70c22963861a582cdef3935a21203a11141a
CompanyWindows <RagdollNoone@163.com>
2019-12-03 17:30:50 +0800

    update java Singleton

--- src/DesignPattern/SingletonPattern/Singleton.java
+++ src/DesignPattern/SingletonPattern/Singleton.java
@@ -1,20 +1,47 @@
 package DesignPattern.SingletonPattern;
 
 public class Singleton {
-    protected static Singleton instance;
-
-    protected Singleton() {}
-
-    public static Singleton getInstance() {
-        if (null == instance) {
-            instance = new Singleton();
-        }
-
-        return instance;
+    private static class SingleHolder {
+        private static Singleton instance = new Singleton();
     }
+
+    private Singleton() {}
+
+    public Singleton getInstance() { return SingleHolder.instance; }
 
     @Override
     public String toString() {
         return "Singleton";
     }
 }
+
+//public class Singleton {
+//    private volatile static Singleton instance;
+//
+//    private Singleton() {}
+//
+//    public static Singleton getInstance() {
+//        if (null == instance) {
+//            synchronized (Singleton.class) {
+//                if (null == instance) {
+//                    instance = new Singleton();
+//                }
+//            }
+//        }
+//
+//        return instance;
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return "Singleton";
+//    }
+//}
+
+//public class Singleton {
+//    private final static Singleton instance = new Singleton();
+//
+//    private Singleton() {}
+//
+//    public Singleton getInstance() { return instance; }
+//}
