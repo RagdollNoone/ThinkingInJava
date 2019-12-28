04c87553bd5870b98eae7233649e9e87d8e46980
UbuntuHome <RagdollNoone@163.com>
2019-12-28 16:13:30 +0800

    add demos

---
+++ src/ThreadBook/SafeListener.java
@@ -0,0 +1,64 @@
+package ThreadBook;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SafeListener {
+    private EventListener listener;
+
+    private static class SafeListenerHolder {
+        private static SafeListener instance = new SafeListener();
+    }
+
+    private SafeListener() {
+        listener = new EventListener() {
+            @Override
+            public void onEvent(Event e) {
+                doSomething();
+            }
+        };
+    }
+
+    public static SafeListener getInstance(EventSource o) {
+        o.registerListener(SafeListenerHolder.instance.listener);
+        return SafeListenerHolder.instance;
+    }
+
+    private void doSomething() {
+        System.out.println("SafeListener doSomething");
+    }
+
+    public static void main(String[] args) {
+        SafeListenerEventSource source = new SafeListenerEventSource();
+        SafeListener.getInstance(source);
+        source.fire();
+    }
+
+    private static class SafeListenerEventSource implements EventSource {
+        List<EventListener> listeners = new ArrayList<>();
+
+        @Override
+        public void registerListener(EventListener o) {
+            listeners.add(o);
+        }
+
+        public void fire() {
+            for (int i = 0; i < listeners.size(); i++) {
+                Event e = new SafeListenerEvent();
+                listeners.get(i).onEvent(e);
+            }
+        }
+    }
+
+    private static class SafeListenerEvent implements Event { }
+
+    interface EventSource {
+        void registerListener(EventListener o);
+    }
+
+    interface EventListener{
+        void onEvent(Event e);
+    }
+
+    interface Event {}
+}
