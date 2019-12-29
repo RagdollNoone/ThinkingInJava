51bc4eab9c69757175bac7d601cc54945f623547
UbuntuHome <RagdollNoone@163.com>
2019-12-29 23:46:55 +0800

    add demos

---
+++ src/ThreadBook/ConnectionDispenser.java
@@ -0,0 +1,55 @@
+package ThreadBook;
+
+import java.util.Random;
+
+public class ConnectionDispenser {
+    private ThreadLocal<Connection> connectionHolder = ThreadLocal.withInitial(() -> {
+        return new Connection();
+    });
+
+    public Connection getConnection() {
+        return connectionHolder.get();
+    }
+
+    public static void main(String[] args) {
+        ConnectionDispenser instance = new ConnectionDispenser();
+        Random random = new Random();
+
+        Runnable r = new Runnable() {
+            @Override
+            public void run() {
+                Thread thd = Thread.currentThread();
+
+                Connection connection = instance.getConnection();
+                System.out.println("Thread name : " + thd.getName() + " connection hashCode : " + connection.hashCode() + " before value : " + Connection.getUrl());
+                Connection.setUrl(String.valueOf(random.nextInt(100)));
+                System.out.println("Thread name : " + thd.getName() + " connection hashCode : " + connection.hashCode() + " after value : " + Connection.getUrl());
+            }
+        };
+
+        for (int i = 0; i < 15; i++) {
+            Thread t = new Thread(r);
+            t.start();
+        }
+    }
+
+    private static class Connection {
+        private static StringBuffer url;
+
+        private Connection() {
+            url = new StringBuffer("-1");
+        }
+
+        public static void setUrl(String s) {
+            if (url.length() > 0) {
+                url.delete(0, url.length() - 1);
+            }
+
+            url.append(s);
+        }
+
+        public static String getUrl() {
+            return url.toString();
+        }
+    }
+}
